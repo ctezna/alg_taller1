df1879deb2d8b55ee063343951b6a490f033fd85
José C. Díaz <jocdiazmuic@gmail.com>
2021-03-26 05:09:33 -0500

    cambios punto taller 13

--- taller1_13.m
+++ taller1_13.m
@@ -1,6 +1,10 @@
-% Calcule el n ́umero condici ́on de la matriz de covarianzas de 
-% portfolio100 en su versi ́on original.Implemente alternativas
-% para mejorarlo y reducirlo a la cuarta parte.
+%&%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% PUNTO 13
+%&%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Calcule el número condición de la matriz de covarianzas de portfolio100 
+% en su versión original. Implemente alternativas para mejorarlo 
+% y reducirlo a la cuarta parte.
+%&%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear;
 
@@ -10,24 +14,33 @@ headerlinesIn = 1;
 Data = importdata(filename,delimiterIn);
 Data = Data(:,2:end);
 C = cov(Data);
-[f c] = size(Data);
-[a b] = size(C);
+[f, c] = size(Data);
+[a, b] = size(C);
 con = cond(C);
-CLW = cov1para(Data);
+[CLW, shrinkage] = cov1para(Data);
 NCLW = cond(Data);
 err = [];
 
-for n=1:500
+target = 0.75
+
+for n=1:1000
+    if n==1
+        ini_cond = cond(cov(Data))
+    end
     x = Data + (eye(f, c) * n);
     cov_n = cov(x);
     err(n) = cond(cov_n);
+    
+    if err(n)<=(1-target)*ini_cond
+        break
+    end
 end
 
-disp(max(err));
+%disp(max(err));
 
 plot(err, 'b');
 change = (con-err(end))/con;
 %title(['Condition number: ', num2str(con),' Condition number final: ', num2str(err(end))]);
-title(['Condition number decrease: ', num2str(change*100), '%']);
-xlabel("Iteration");
-ylabel("Condition number");
+title(['Porcentaje de decrecimiento: ', num2str(change*100), '%', '- objetivo: ',num2str(target*100),'%']);
+xlabel("Iteraciones");
+ylabel("Numero condicion");
